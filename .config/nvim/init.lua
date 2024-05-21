b41b2df2a6b84218b93eeb235ea06f5a182661e6
Hristo Karamanliev <karamanliev@gmail.com>
2024-05-21 17:01:51 +0300

    add new motions category and add surround

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -157,15 +157,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'kylechui/nvim-surround',
-    version = '*', -- Use for stability; omit to use `main` branch for the latest features
-    event = 'VeryLazy',
-    config = function()
-      require('nvim-surround').setup {}
-    end,
-  },
-
   { import = 'plugins' },
 }, {
   ui = {
