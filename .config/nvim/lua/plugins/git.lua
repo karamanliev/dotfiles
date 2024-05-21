56989103cd942bf02981198ecaa6a3c675420a86
Hristo Karamanliev <karamanliev@gmail.com>
2024-05-21 18:30:08 +0300

    configure more keymaps for diffview

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -79,7 +79,39 @@ return {
     'sindrets/diffview.nvim',
     cmd = 'DiffviewOpen',
     keys = {
-      { '<leader>ld', '<cmd>DiffviewOpen<cr>', desc = '[D]iffview' },
+      { '<leader>ld', '<cmd>DiffviewOpen<cr>', desc = 'Workspace [D]iff View' },
+      { '<leader>lf', '<cmd>DiffviewFileHistory %<cr>', desc = '[F]ile Diff View' },
     },
+    opts = function()
+      local actions = require 'diffview.actions'
+
+      return {
+        enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
+        keymaps = {
+          view = {
+            { 'n', 'q', actions.close },
+            { 'n', '<Tab>', actions.select_next_entry },
+            { 'n', '<S-Tab>', actions.select_prev_entry },
+            -- { 'n', '<LocalLeader>a', actions.focus_files },
+            -- { 'n', '<LocalLeader>e', actions.toggle_files },
+          },
+          file_panel = {
+            { 'n', 'q', actions.close },
+            -- { 'n', 'h', actions.prev_entry },
+            -- { 'n', 'o', actions.focus_entry },
+            -- { 'n', 'gf', actions.goto_file },
+            -- { 'n', 'sg', actions.goto_file_split },
+            -- { 'n', 'st', actions.goto_file_tab },
+            -- { 'n', '<C-r>', actions.refresh_files },
+            -- { 'n', '<LocalLeader>e', actions.toggle_files },
+          },
+          file_history_panel = {
+            { 'n', 'q', '<cmd>DiffviewClose<CR>' },
+            -- { 'n', 'o', actions.focus_entry },
+            -- { 'n', 'O', actions.options },
+          },
+        },
+      }
+    end,
   },
 }
