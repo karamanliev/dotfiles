76f97bf1daa78550d9e57fb8d7161c3195b1aa04
Hristo Karamanliev <karamanliev@gmail.com>
2024-05-21 17:01:33 +0300

    move noice and scroll to ui

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -76,4 +76,83 @@ return {
       }
     end,
   },
+
+  -- Scrollbar
+  {
+    'petertriho/nvim-scrollbar',
+    event = 'VeryLazy',
+    dependencies = { { 'kevinhwang91/nvim-hlslens' } },
+    config = function()
+      local colors = require('tokyonight.colors').setup()
+
+      require('scrollbar').setup {
+        handle = {
+          color = colors.bg_highlight,
+        },
+        marks = {
+          Search = { color = colors.orange },
+          Error = { color = colors.error },
+          Warn = { color = colors.warning },
+          Info = { color = colors.info },
+          Hint = { color = colors.hint },
+          Misc = { color = colors.purple },
+          GitAdd = { color = colors.info },
+        },
+        handlers = {
+          cursor = false,
+          search = true,
+          gitsigns = true,
+        },
+      }
+    end,
+  },
+
+  -- Noice (nice cmd/search line ui + notifications)
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      -- 'rcarriga/nvim-notify',
+    },
+    keys = {
+      {
+        '<leader>ll',
+        function()
+          require('noice').cmd 'telescope'
+        end,
+        desc = 'List [L]ogs',
+      },
+    },
+    config = function()
+      require('noice').setup {
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+          },
+          progress = {
+            enabled = false,
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = false, -- use a classic bottom cmdline for search
+          command_palette = false, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+        messages = {
+          view_search = false,
+        },
+      }
+    end,
+  },
 }
