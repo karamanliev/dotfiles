fc38bfe2e8e074d19e510bec266f1fb7e61f2047
Hristo Karamanliev <karamanliev@gmail.com>
2024-05-21 17:54:49 +0300

    cleanup init and add plugins to motions.lua

--- .config/nvim/lua/plugins/motions.lua
+++ .config/nvim/lua/plugins/motions.lua
@@ -1,4 +1,44 @@
 return {
+  -- Visualizie motions
+  {
+    'folke/which-key.nvim',
+    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    config = function() -- This is the function that runs, AFTER loading
+      require('which-key').setup()
+
+      -- Document existing key chains
+      require('which-key').register {
+        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
+        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        ['<leader>l'] = { name = '[L]ist', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      }
+      -- visual mode
+      require('which-key').register({
+        ['<leader>h'] = { 'Git [H]unk' },
+      }, { mode = 'v' })
+    end,
+  },
+
+  -- Comment motions
+  {
+    'numToStr/Comment.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+    config = function()
+      local ts_context_commentstring = require 'ts_context_commentstring.integrations.comment_nvim'
+      ---@diagnostic disable-next-line: missing-fields
+      require('Comment').setup {
+        pre_hook = ts_context_commentstring.create_pre_hook(),
+      }
+    end,
+  },
+
+  -- Surround motions
   {
     'kylechui/nvim-surround',
     version = '*', -- Use for stability; omit to use `main` branch for the latest features
@@ -8,6 +48,7 @@ return {
     end,
   },
 
+  -- Subsitute motions
   {
     'gbprod/substitute.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
