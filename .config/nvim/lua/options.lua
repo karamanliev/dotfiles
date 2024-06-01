4a120673481cf9ba6a1cdb4bd2f83be463886519
Hristo Karamanliev <karamanliev@gmail.com>
2024-06-01 13:21:59 +0300

    change winblend values

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -69,8 +69,8 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- set transparency to the completion and popup menus
-vim.opt.pumblend = 15
-vim.opt.winblend = 15
+vim.opt.pumblend = 10
+vim.opt.winblend = 10
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 15
