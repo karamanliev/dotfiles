c2aac8c2c2b56d7213801746fbe7d384cb1db34f
Hristo Karamanliev <karamanliev@gmail.com>
2024-06-13 12:06:50 +0300

    skip oil simple edits confirm

--- .config/nvim/lua/plugins/files.lua
+++ .config/nvim/lua/plugins/files.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       require('oil').setup({
         default_file_explorer = true,
+        skip_confirm_for_simple_edits = true,
         view_options = {
           show_hidden = true,
         },
