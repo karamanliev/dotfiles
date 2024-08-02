c9f29e8435f75a5bb6c87ff3242a4c4ce0213aef
Hristo Karamanliev <karamanliev@gmail.com>
2024-08-02 17:34:38 +0300

    improve notifications of gpu-screen-recorder scripts

--- .local/bin/gpu-screen-recorder-replay-save.sh
+++ .local/bin/gpu-screen-recorder-replay-save.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -e
 
-killall -SIGUSR1 gpu-screen-recorder && sleep 0.5 && notify-send -a "GPU Screen Recorder" -i camera-video-symbolic "Replay saved."
+video_path="$HOME/Videos/Recordings/Replays"
+killall -SIGUSR1 gpu-screen-recorder && sleep 0.5 && notify-send -a "GPU Screen Recorder" -i camera-video-symbolic "Replay saved." "Saving video in $video_path"
--- .local/bin/gpu-screen-recorder-replay-start.sh
+++ .local/bin/gpu-screen-recorder-replay-start.sh
@@ -2,5 +2,5 @@
 pidof -q gpu-screen-recorder && exit 1
 video_path="$HOME/Videos/Recordings/Replays"
 mkdir -p "$video_path"
-notify-send --hint=int:transient:1 -a "GPU Screen Recorder" -i media-record-symbolic "Replay recording service has started."
+notify-send --hint=int:transient:1 -i media-record-symbolic -a "GPU Screen Recorder" "Replay recording service has started." "Save the last 45 seconds with Super+Shift+F5."
 gpu-screen-recorder -w "DP-3" -f 60 -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -c mp4 -r 45 -o "$video_path"
