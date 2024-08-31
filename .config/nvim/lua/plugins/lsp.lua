da1e63a6fdd5edf1e6a850896208fef7494c9eae
Hristo Karamanliev <hkaramanliev@despark.com>
2024-08-31 18:19:00 +0300

    add handler for codelens for vtsls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -345,6 +345,17 @@ return {
               },
             },
           },
+          handlers = {
+            ['editor.action.showReferences'] = function(command, ctx)
+              local locations = command.arguments[3]
+              local client = vim.lsp.get_client_by_id(ctx.client_id)
+              if locations and #locations > 0 then
+                local items = vim.lsp.util.locations_to_items(locations, client.offset_encoding)
+                vim.fn.setloclist(0, {}, ' ', { title = 'References', items = items, context = ctx })
+                vim.api.nvim_command('lopen')
+              end
+            end,
+          },
         },
         --[[ tsserver = {
           enabled = false,
