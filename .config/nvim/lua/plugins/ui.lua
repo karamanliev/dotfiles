22ee61754c1ba7525e8e5ea05281c6f0c89f2afc
Hristo Karamanliev <hkaramanliev@despark.com>
2024-09-07 02:22:21 +0300

    add next/prev todo

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -87,6 +87,22 @@ return {
   {
     'folke/todo-comments.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
+    keys = {
+      {
+        ']t',
+        function()
+          require('todo-comments').jump_next()
+        end,
+        desc = 'Next TODO comment',
+      },
+      {
+        '[t',
+        function()
+          require('todo-comments').jump_prev()
+        end,
+        desc = 'Previous TODO comment',
+      },
+    },
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = false },
   },
