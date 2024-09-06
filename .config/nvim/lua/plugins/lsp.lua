7a8dfb938730f913e9389d28e328a424d78bdab8
Hristo Karamanliev <hkaramanliev@despark.com>
2024-09-07 01:56:34 +0300

    use vim-illuminate for highlighting references instead of autocmd

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -165,7 +165,7 @@ return {
           end, 'Mega Hover') ]]
 
           -- Highlight references
-          if client and client.server_capabilities.documentHighlightProvider then
+          --[[ if client and client.server_capabilities.documentHighlightProvider then
             local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = event.buf,
@@ -186,7 +186,7 @@ return {
                 vim.api.nvim_clear_autocmds({ group = 'lsp-highlight', buffer = event.buf })
               end,
             })
-          end
+          end ]]
 
           -- Toggle inlay hints
           if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
@@ -845,5 +845,43 @@ return {
         end,
       })
     end,
+  },
+
+  -- Illuminate the current word under the cursor
+  {
+    'RRethy/vim-illuminate',
+    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+    opts = {
+      delay = 200,
+      large_file_cutoff = 2000,
+      large_file_overrides = {
+        providers = { 'lsp' },
+      },
+    },
+    config = function(_, opts)
+      require('illuminate').configure(opts)
+
+      local function map(key, dir, buffer)
+        vim.keymap.set('n', key, function()
+          require('illuminate')['goto_' .. dir .. '_reference'](false)
+        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. ' Reference', buffer = buffer })
+      end
+
+      map(']]', 'next')
+      map('[[', 'prev')
+
+      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          local buffer = vim.api.nvim_get_current_buf()
+          map(']]', 'next', buffer)
+          map('[[', 'prev', buffer)
+        end,
+      })
+    end,
+    keys = {
+      { ']]', desc = 'Next Reference' },
+      { '[[', desc = 'Prev Reference' },
+    },
   },
 }
