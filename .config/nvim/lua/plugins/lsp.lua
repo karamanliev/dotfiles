b825ef7402e07f9c10ea83b139d4bed7354bd8ed
Hristo Karamanliev <hkaramanliev@despark.com>
2024-09-23 19:58:18 +0300

    add nil check

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -652,7 +652,7 @@ return {
       vim.lsp.commands['editor.action.showReferences'] = function(command, ctx)
         local locations = command.arguments[3]
         local client = vim.lsp.get_client_by_id(ctx.client_id)
-        if locations and #locations > 0 then
+        if client and locations and #locations > 0 then
           local items = vim.lsp.util.locations_to_items(locations, client.offset_encoding)
           vim.fn.setloclist(0, {}, ' ', { title = 'References', items = items, context = ctx })
           vim.api.nvim_command('lopen')
