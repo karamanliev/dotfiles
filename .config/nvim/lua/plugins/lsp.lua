d923e33713941fbf7ea2c0dcb42e6d7da503105a
Hristo Karamanliev <karamanliev@gmail.com>
2024-09-23 19:53:30 +0300

    disable vtsls formatter

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -361,6 +361,10 @@ return {
               },
             },
           },
+          on_attach = function(client, bufnr)
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+          end,
         },
         eslint = {
           settings = {
