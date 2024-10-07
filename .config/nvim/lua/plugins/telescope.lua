a1ecf6581cfb90b77d0c047ccea3d2d6985f951f
Hristo Karamanliev <karamanliev@gmail.com>
2024-10-07 20:06:25 +0300

    update telescope to latest

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,6 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     event = { 'VeryLazy' },
-    branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -125,6 +124,7 @@ return {
           },
           colorscheme = {
             enable_preview = true,
+            ignore_builtins = true,
             mappings = {
               i = {
                 ['<CR>'] = misc.set_colorscheme,
