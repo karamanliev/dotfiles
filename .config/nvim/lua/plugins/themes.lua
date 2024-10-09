ebe4546fefccffc31a9e931dcfda6b77d27319f8
Hristo Karamanliev <karamanliev@gmail.com>
2024-10-09 11:08:52 +0300

    add kanagawa themes

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -167,4 +167,62 @@ return {
       })
     end,
   },
+
+  {
+    'rebelot/kanagawa.nvim',
+    lazy = true,
+    priority = 1000,
+    keys = require('utils.misc').theme_switch_kb,
+    config = function()
+      require('kanagawa').setup({
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = 'dragon', -- Load "wave" theme when 'background' option is not set
+        background = { -- map the value of 'background' option to a theme
+          dark = 'dragon', -- try "dragon" !
+          light = 'lotus',
+        },
+      })
+    end,
+  },
+
+  {
+    'sho-87/kanagawa-paper.nvim',
+    lazy = true,
+    priority = 1000,
+    keys = require('utils.misc').theme_switch_kb,
+    config = function()
+      require('kanagawa-paper').setup({
+        undercurl = true,
+        transparent = false,
+        gutter = false,
+        dimInactive = false, -- disabled when transparent
+        terminalColors = true,
+        commentStyle = { italic = true },
+        functionStyle = { italic = false },
+        keywordStyle = { italic = false, bold = false },
+        statementStyle = { italic = false, bold = false },
+        typeStyle = { italic = false },
+        colors = { theme = {}, palette = {} }, -- override default palette and theme colors
+        overrides = function() -- override highlight groups
+          return {}
+        end,
+      })
+    end,
+  },
 }
