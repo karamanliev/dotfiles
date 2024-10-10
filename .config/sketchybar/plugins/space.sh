6e1f4d1a69682c263ebe076a20d05a718565a2f2
Hristo Karamanliev <hkaramanliev@despark.com>
2024-10-10 22:59:50 +0300

    update sketchybar and aerospace

--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -2,6 +2,7 @@
 
 source "$HOME/.config/sketchybar/variables.sh" # Loads all defined colors
 
+ICONS=("1" "2" "3" "4" "5" "6" "C")
 SPACE_CLICK_SCRIPT="aerospace workspace $SID"
 
 EMPTY_WORKSPACES=$(aerospace list-workspaces --monitor all --empty)
@@ -14,13 +15,13 @@ fi
 
 if [ $SID = "$FOCUSED_WORKSPACE" ]; then
 	sketchybar --animate tanh 5 --set "$NAME" \
-		icon="$SID" \
+		icon="${ICONS[$SID - 1]}" \
 		icon.color="$ICON_COLOR" \
 		background.drawing=on \
 		click_script="$SPACE_CLICK_SCRIPT"
 else
 	sketchybar --animate tanh 5 --set "$NAME" \
-		icon="$SID" \
+		icon="${ICONS[$SID - 1]}" \
 		icon.color="$ICON_COLOR" \
 		background.drawing=off \
 		click_script="$SPACE_CLICK_SCRIPT"
