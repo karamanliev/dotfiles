773d2eac36f38c0ac6637a1a95facabbaabb8b6a
Hristo Karamanliev <hkaramanliev@despark.com>
2024-11-19 01:47:36 +0200

    feat(ui): include barbecue.nvim as a disabled plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -545,4 +545,36 @@ return {
       require('force-cul').setup()
     end,
   },
+
+  {
+    'utilyre/barbecue.nvim',
+    enabled = false,
+    name = 'barbecue',
+    version = '*',
+    dependencies = {
+      'SmiteshP/nvim-navic',
+      'nvim-tree/nvim-web-devicons', -- optional dependency
+    },
+    config = function()
+      require('barbecue').setup({
+        create_autocmd = false, -- prevent barbecue from updating itself automatically
+        show_modified = true, -- show modified indicators in the bar
+      })
+
+      vim.api.nvim_create_autocmd({
+        'WinScrolled', -- or WinResized on NVIM-v0.9 and higher
+        'BufWinEnter',
+        'CursorHold',
+        'InsertLeave',
+
+        -- include this if you have set `show_modified` to `true`
+        'BufModifiedSet',
+      }, {
+        group = vim.api.nvim_create_augroup('barbecue.updater', {}),
+        callback = function()
+          require('barbecue.ui').update()
+        end,
+      })
+    end,
+  },
 }
