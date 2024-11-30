397efff618dfcff672f722d319d698d16b54fc81
Hristo Karamanliev <karamanliev@gmail.com>
2024-11-30 13:47:07 +0200

    feat(wallpaper): add random and path wallpaper selection feature

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WP_FOLDER="$HOME/Pictures/Wallpapers/Ultrawide/"
+WP_FOLDER="$HOME/Pictures/Wallpapers/Ultrawide"
 WP_INDEX_FILE="$HOME/.cache/rwpspread/current_wallpaper_index"
 
 if [ ! -f "$WP_INDEX_FILE" ]; then
@@ -15,6 +15,20 @@ load_wallpapers() {
 get_current_wallpaper() {
   CURRENT_INDEX=$(cat "$WP_INDEX_FILE")
   CURRENT_WALLPAPER="${WALLPAPERS_ARR[$CURRENT_INDEX]}"
+}
+
+get_wallpaper_index_by_path() {
+  local file_path="$1"
+  local file_name=$(basename "$file_path")
+
+  for i in "${!WALLPAPERS_ARR[@]}"; do
+    if [ "${WALLPAPERS_ARR[$i]}" = "$file_name" ]; then
+      echo "$i"
+      return 0
+    fi
+  done
+  echo "-1"
+  return 1
 }
 
 if [ "$1" = "daemon" ]; then
@@ -34,6 +48,25 @@ else
     CURRENT_INDEX=$(((CURRENT_INDEX + 1) % WALLPAPERS_COUNT))
   elif [ "$1" = "prev" ]; then
     CURRENT_INDEX=$(((CURRENT_INDEX - 1 + WALLPAPERS_COUNT) % WALLPAPERS_COUNT))
+  elif [ "$1" = "random" ]; then
+    RANDOM_INDEX=$((RANDOM % WALLPAPERS_COUNT))
+    # Ensure the random index is different from the current one
+    while [ "$RANDOM_INDEX" -eq "$CURRENT_INDEX" ]; do
+      RANDOM_INDEX=$((RANDOM % WALLPAPERS_COUNT))
+    done
+    CURRENT_INDEX=$RANDOM_INDEX
+  # If the argument is path to a file
+  elif [ -f "$1" ]; then
+    NEW_INDEX=$(get_wallpaper_index_by_path "$1")
+    if [ "$NEW_INDEX" -ge 0 ]; then
+      CURRENT_INDEX="$NEW_INDEX"
+    else
+      echo "Error: Wallpaper '$1' not found in '$WP_FOLDER'."
+      exit 1
+    fi
+  else
+    echo "Error: Invalid argument or file does not exist."
+    exit 1
   fi
 
   echo "$CURRENT_INDEX" >"$WP_INDEX_FILE"
