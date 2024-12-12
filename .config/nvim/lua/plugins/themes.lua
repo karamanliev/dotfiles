f6a404069dd0351288ac6faf5b7d53701eae3d37
Hristo Karamanliev <hkaramanliev@despark.com>
2024-12-12 11:33:34 +0200

    remove minimal theme (missing from gh)

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -363,20 +363,6 @@ return {
   },
 
   {
-    'Yazeed1s/minimal.nvim',
-    priority = 1000,
-    lazy = true,
-    keys = keys,
-    config = function()
-      vim.g.minimal_italic_keywords = true
-      vim.g.minimal_italic_functions = true
-      vim.g.minimal_italic_booleans = true
-      vim.g.minimal_italic_comments = true
-      vim.g.minimal_italic_variables = true
-    end,
-  },
-
-  {
     'ramojus/mellifluous.nvim',
     priority = 1000,
     lazy = true,
