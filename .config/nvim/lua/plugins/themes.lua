7c599f084203e93ecefd3ad61541de9b0b22ed29
Hristo Karamanliev <karamanliev@gmail.com>
2025-01-29 22:34:29 +0200

    add oldworld theme

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -464,4 +464,50 @@ return {
       })
     end,
   },
+
+  {
+    'dgox16/oldworld.nvim',
+    lazy = true,
+    priority = 1000,
+    keys = keys,
+    config = function()
+      local adjust_hex = require('utils.misc').adjust_hex_brightness
+
+      require('oldworld').setup({
+        terminal_colors = true, -- enable terminal colors
+        variants = 'default', -- Two variants availables: default and cooler
+        styles = { -- You can pass the style using the format: style = true
+          comments = {}, -- style for comments
+          keywords = {}, -- style for keywords
+          identifiers = {}, -- style for identifiers
+          functions = {}, -- style for functions
+          variables = {}, -- style for variables
+          booleans = {}, -- style for booleans
+        },
+        integrations = { -- You can disable/enable integrations
+          alpha = true,
+          cmp = true,
+          flash = true,
+          gitsigns = true,
+          hop = false,
+          indent_blankline = true,
+          lazy = true,
+          lsp = true,
+          markdown = true,
+          mason = true,
+          navic = false,
+          neo_tree = false,
+          neorg = false,
+          noice = true,
+          notify = true,
+          rainbow_delimiters = true,
+          telescope = true,
+          treesitter = true,
+        },
+        highlight_overrides = {
+          CursorLine = { bg = adjust_hex('#1b1b1c', 'lighten', 25) },
+        },
+      })
+    end,
+  },
 }
