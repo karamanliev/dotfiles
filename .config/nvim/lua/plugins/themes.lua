ced4ee5961fcadfcdffa84baa741be78dfbbb721
Hristo Karamanliev <karamanliev@gmail.com>
2025-01-31 23:45:14 +0200

    update themes

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -474,36 +474,7 @@ return {
       local adjust_hex = require('utils.misc').adjust_hex_brightness
 
       require('oldworld').setup({
-        terminal_colors = true, -- enable terminal colors
-        variants = 'default', -- Two variants availables: default and cooler
-        styles = { -- You can pass the style using the format: style = true
-          comments = {}, -- style for comments
-          keywords = {}, -- style for keywords
-          identifiers = {}, -- style for identifiers
-          functions = {}, -- style for functions
-          variables = {}, -- style for variables
-          booleans = {}, -- style for booleans
-        },
-        integrations = { -- You can disable/enable integrations
-          alpha = true,
-          cmp = true,
-          flash = true,
-          gitsigns = true,
-          hop = false,
-          indent_blankline = true,
-          lazy = true,
-          lsp = true,
-          markdown = true,
-          mason = true,
-          navic = false,
-          neo_tree = false,
-          neorg = false,
-          noice = true,
-          notify = true,
-          rainbow_delimiters = true,
-          telescope = true,
-          treesitter = true,
-        },
+        variants = 'default', -- default, oled, cooler
         highlight_overrides = {
           CursorLine = { bg = adjust_hex('#1b1b1c', 'lighten', 25) },
           Folded = { bg = adjust_hex('#1b1b1c', 'lighten', 10) },
