a2b06c7ef5d473866f4d03415d593b8d4354dae2
Hristo Karamanliev <hkaramanliev@despark.com>
2025-02-20 10:33:02 +0200

    add scroll events for vol up/down on sketchybar

--- .config/sketchybar/items/volume.sh
+++ .config/sketchybar/items/volume.sh
@@ -3,12 +3,12 @@
 COLOR="$WHITE"
 
 sketchybar \
-	--add item sound right \
-	--set sound \
-	icon.color="$COLOR" \
-	icon.padding_left=10 \
-	label.color="$COLOR" \
-	label.padding_right=2 \
-	script="$PLUGIN_DIR/sound.sh" \
-	click_script="aerospace volume mute-toggle" \
-	--subscribe sound volume_change
+  --add item sound right \
+  --set sound \
+  icon.color="$COLOR" \
+  icon.padding_left=10 \
+  label.color="$COLOR" \
+  label.padding_right=2 \
+  script="$PLUGIN_DIR/sound.sh" \
+  click_script="aerospace volume mute-toggle" \
+  --subscribe sound volume_change mouse.scrolled
--- .config/sketchybar/plugins/sound.sh
+++ .config/sketchybar/plugins/sound.sh
@@ -3,18 +3,24 @@
 VOLUME=$(osascript -e "output volume of (get volume settings)")
 MUTED=$(osascript -e "output muted of (get volume settings)")
 
+if [ "$SCROLL_DELTA" -lt 0 ]; then
+  aerospace volume down
+elif [ "$SCROLL_DELTA" -gt 0 ]; then
+  aerospace volume up
+fi
+
 if [ "$MUTED" != "false" ]; then
-	ICON="󰖁"
-	VOLUME=0
+  ICON="󰖁"
+  VOLUME=0
 else
-	case ${VOLUME} in
-	100) ICON="" ;;
-	[5-9]*) ICON="" ;;
-	[0-9]*) ICON="" ;;
-	*) ICON="" ;;
-	esac
+  case ${VOLUME} in
+  100) ICON="" ;;
+  [5-9]*) ICON="" ;;
+  [0-9]*) ICON="" ;;
+  *) ICON="" ;;
+  esac
 fi
 
 sketchybar -m \
-	--set "$NAME" icon=$ICON \
-	--set "$NAME" label="$VOLUME%"
+  --set "$NAME" icon=$ICON \
+  --set "$NAME" label="$VOLUME%"
