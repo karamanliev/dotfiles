5e750c12a001de3551df20d723ae4eb748e245c3
Hristo Karamanliev <karamanliev@gmail.com>
2025-02-21 17:49:53 +0200

    more waybar config

---
+++ .config/waybar/scripts/vpn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+state=$(nmcli connection show Despark | grep -oE '[0-9]+ - (VPN connecting|VPN connected)')
+
+if echo "$state" | grep -qi 'connecting'; then
+  echo "󱂇  Connecting..."
+elif echo "$state" | grep -qi 'connected'; then
+  echo "󰱓  On"
+else
+  echo "󰅛  Off"
+fi
