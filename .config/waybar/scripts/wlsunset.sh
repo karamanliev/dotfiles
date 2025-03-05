285a318dc77a238dd461d0aac2322b5bbe184d7e
Hristo Karamanliev <karamanliev@gmail.com>
2025-03-05 14:40:43 +0200

    update how wlsunset works

--- .config/waybar/scripts/wlsunset.sh
+++ .config/waybar/scripts/wlsunset.sh
@@ -3,24 +3,10 @@ if pgrep wlsunset >/dev/null 2>&1; then
   ddcutil --noverify --bus 1 setvcp 10 100 &
   ddcutil --noverify --bus 4 setvcp 10 100 &
 else
-  RETRIES=30
-  counter=0
-  while true; do
-    CONTENT=$(curl -s http://ip-api.com/json/)
-    if [ $? -eq 0 ]; then
-      break
-    fi
-    counter=$((counter + 1))
-    if [ $counter -eq $RETRIES ]; then
-      notify-send wlsunset.sh "Unable to connect to ip-api."
-      break
-    fi
-    sleep 2
-  done
-  longitude=$(echo $CONTENT | jq .lon)
-  latitude=$(echo $CONTENT | jq .lat)
+  longitude="27.910543"
+  latitude="43.204666"
   wlsunset -l $latitude -L $longitude >/dev/null 2>&1 &
-  ddcutil --noverify --bus 1 setvcp 10 70 &
-  ddcutil --noverify --bus 4 setvcp 10 70 &
+  ddcutil --noverify --bus 1 setvcp 10 75 &
+  ddcutil --noverify --bus 4 setvcp 10 75 &
 fi
 pkill -35 waybar
