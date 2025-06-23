67112c92108eee78af6fb31b9c9968c1ac271510
Hristo Karamanliev <karamanliev@gmail.com>
2025-06-23 22:47:13 +0300

    update resize_cnter_floating script to handle portrait monitor and launch windows

--- .config/hypr/scripts/resize_center_floating.sh
+++ .config/hypr/scripts/resize_center_floating.sh
@@ -1,22 +1,55 @@
 #!/usr/bin/env bash
 
+monitor_data=$(hyprctl monitors -j)
+
+function get_monitor_and_transform {
+  local real_address="$1"
+  local active_monitor
+  active_monitor=$(hyprctl clients -j | jq -r ".[] | select(.address == \"$real_address\").monitor")
+  transform=$(echo "$monitor_data" | jq -r ".[] | select(.id == $active_monitor).transform")
+}
+
+function resize_and_center {
+  local real_address="$1"
+  local transform="$2"
+
+  if [ "$transform" != 0 ]; then
+    hyprctl --batch "\
+      dispatch resizewindowpixel exact 96% 40%,address:${real_address};\
+      dispatch centerwindow"
+  else
+    hyprctl --batch "\
+      dispatch resizewindowpixel exact 65% 60%,address:${real_address};\
+      dispatch centerwindow"
+  fi
+}
+
 function handle {
   case $1 in
+  openwindow*)
+    args="${1#openwindow>>}"
+    address="${args%%,*}"
+    real_address="0x$address"
+
+    get_monitor_and_transform "$real_address"
+    floating=$(hyprctl clients -j | jq -r ".[] | select(.address == \"$real_address\").floating")
+
+    if [ "$floating" = true ]; then
+      resize_and_center "$real_address" "$transform"
+    fi
+    ;;
   changefloatingmode*)
     args="${1#changefloatingmode>>}"
-
     address="${args%,*}"
     floating="${args#*,}"
     real_address="0x$address"
 
     if [ "$floating" = 1 ]; then
-      hyprctl --batch "\
-        dispatch resizewindowpixel exact 65% 65%,address:${real_address};\
-        dispatch centerwindow"
+      get_monitor_and_transform "$real_address"
+      resize_and_center "$real_address" "$transform"
     fi
     ;;
   esac
-
 }
 
 socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
