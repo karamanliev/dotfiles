a585996432ad029f312acadda3671cc242f78b27
Hristo Karamanliev <karamanliev@gmail.com>
2025-08-15 14:39:57 +0300

    cleanup whichkey mappings

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -22,8 +22,6 @@ return {
         { '<leader>d', group = 'Diff' },
         { '<leader>s', group = 'Session' },
         { '<leader>t', group = 'Toggle' },
-        { '<leader>y', group = 'Yank' },
-        { '<leader>a', group = 'AI' },
       })
       -- visual mode
       wk.add({
@@ -31,31 +29,9 @@ return {
       })
 
       wk.add({
-        { '\\D', '<cmd>bd!<CR>', desc = 'Buffer delete without saving' },
-        { '\\C', '<cmd>close!<CR>', desc = 'Close without saving' },
-        { '\\Q', '<cmd>q!<CR>', desc = 'Quit without saving' },
-        { '\\X', '<cmd>qa!<CR>', desc = 'Quit all without saving' },
         { '\\Z', '<cmd>ClearBuffers!<CR>', desc = 'Clear all buffers (unsaved also), but this one' },
-        { '\\a', '<cmd>e #<CR>', desc = 'Last buffer' },
-        { '\\d', '<cmd>bd<CR>', desc = 'Buffer delete' },
-        { '\\c', '<cmd>close<CR>', desc = 'Close' },
-        { '\\q', '<cmd>q<CR>', desc = 'Quit' },
-        { '\\w', '<cmd>w<CR>', desc = 'Write' },
-        { '\\A', '<cmd>wa<CR>', desc = 'Write All Unsaved Buffers' },
         { '\\W', '<cmd>WriteWithoutFormat<CR>', desc = 'Write (No Autoformat)' },
-        { '\\x', '<cmd>qa<CR>', desc = 'Quit all' },
         { '\\z', '<cmd>ClearBuffers<CR>', desc = 'Clear all buffers, but this one' },
-      })
-
-      wk.add({
-        { '\\H', '<C-w>H', desc = 'Move window to left' },
-        { '\\J', '<C-w>J', desc = 'Move window to bottom' },
-        { '\\K', '<C-w>K', desc = 'Move window to top' },
-        { '\\L', '<C-w>L', desc = 'Move window to right' },
-        { '\\h', '<cmd>leftabove vsplit<CR>', desc = 'Split vertical left' },
-        { '\\j', '<cmd>split<CR>', desc = 'Split horizontal down' },
-        { '\\k', '<cmd>top split<CR>', desc = 'Split horizontal up' },
-        { '\\l', '<cmd>vsplit<CR>', desc = 'Split vertical right' },
       })
     end,
   },
