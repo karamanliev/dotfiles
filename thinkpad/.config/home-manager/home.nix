2a204512f52bd7ef0a8dfb22706d08bd71521b5b
Hristo Karamanliev <hi@hristo.dev>
2026-02-19 20:10:47 +0200

    add home-manager config

---
+++ thinkpad/.config/home-manager/home.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "hristo.karamanliev";
+  home.homeDirectory = "/home/hristo.karamanliev";
+
+  nix = {
+   package = pkgs.nix;
+   settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  home.stateVersion = "25.11";
+
+  targets.genericLinux.enable = true;
+
+  programs.zsh.enable = false;
+
+  home.packages = with pkgs; [
+    alacritty
+    atuin
+    bat
+    blueman
+    brightnessctl
+    btop
+    delta
+    eza
+    fd
+    fnm
+    fzf
+    kitty
+    lazydocker
+    lazygit
+    neovim
+    nerd-fonts.symbols-only
+    niri
+    pavucontrol
+    power-profiles-daemon
+    ripgrep
+    satty
+    sesh
+    starship
+    sunsetr
+    swaynotificationcenter
+    tealdeer
+    telegram-desktop
+    tmux
+    ungoogled-chromium
+    vicinae
+    waybar
+    wpaperd
+    yazi
+    zoxide
+    zsh-autosuggestions
+    zsh-completions
+    zsh-fast-syntax-highlighting
+    (zsh-autocomplete.overrideAttrs (old: {
+      installPhase = ''
+        install -D zsh-autocomplete.plugin.zsh \
+          $out/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+        cp -R Completions $out/share/zsh/plugins/zsh-autocomplete/Completions
+        cp -R Functions $out/share/zsh/plugins/zsh-autocomplete/Functions
+        ln -s $out/share/zsh/plugins/zsh-autocomplete \
+          $out/share/zsh-autocomplete
+      '';
+    }))
+  ];
+
+  programs.home-manager.enable = true;
+}
