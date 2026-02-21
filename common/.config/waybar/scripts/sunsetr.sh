b03acf4c981e61f8593424ab8e2f29fc9695dc87
Hristo Karamanliev <hi@hristo.dev>
2026-02-21 19:07:52 +0200

    fix waybar script not setting gtk-theme

--- common/.config/waybar/scripts/sunsetr.sh
+++ common/.config/waybar/scripts/sunsetr.sh
@@ -9,9 +9,11 @@ set_gtk_theme() {
   local period="$1"
   case "$period" in
     day|sunset)
+      gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
       gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
       ;;
     night|sunrise)
+      gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
       gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
       ;;
   esac
