c241264125890e167f011ef1e698fa01b558a176
Hristo Karamanliev <hi@hristo.dev>
2026-05-08 20:49:24 +0300

    add fzf gruvbox-material themes

---
+++ common/.config/fzf/themes/gruvbox-material-dark.sh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+--color=bg+:#45403d,bg:#282828,spinner:#d3869b,hl:#ea6962 \
+--color=fg:#d4be98,header:#ea6962,info:#7daea3,pointer:#d3869b \
+--color=marker:#d8a657,fg+:#d4be98,prompt:#7daea3,hl+:#ea6962 \
+--color=selected-bg:#4f422e \
+--color=border:#5a524c,label:#d4be98"
---
+++ common/.config/fzf/themes/gruvbox-material-light.sh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+--color=bg+:#eee0b7,bg:#fbf1c7,spinner:#945e80,hl:#c14a4a \
+--color=fg:#654735,header:#c14a4a,info:#45707a,pointer:#945e80 \
+--color=marker:#b47109,fg+:#654735,prompt:#45707a,hl+:#c14a4a \
+--color=selected-bg:#fae7b3 \
+--color=border:#ddccab,label:#654735"
