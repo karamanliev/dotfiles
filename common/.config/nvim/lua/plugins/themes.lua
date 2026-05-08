01eed8b5be8e5018fc002e2a944f110f43877841
Hristo Karamanliev <hi@hristo.dev>
2026-05-08 19:45:30 +0300

    add gruvbox-material to nvim

--- common/.config/nvim/lua/plugins/themes.lua
+++ common/.config/nvim/lua/plugins/themes.lua
@@ -1,5 +1,14 @@
 return {
   {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme('gruvbox-material')
+    end,
+  },
+  {
     'folke/tokyonight.nvim',
     enabled = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
